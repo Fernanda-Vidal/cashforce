3e79f92c76e25b1e13cc77d4478e1300d8021335
Fernanda Vidal <fevidal.dev@gmail.com>
2022-12-28 16:26:22 -0300

    fix: Lint.

--- backend/src/database/seeders/20221228143848-cnpjs.js
+++ backend/src/database/seeders/20221228143848-cnpjs.js
@@ -6,14 +6,14 @@ module.exports = {
         cnpj: '00000000000001',
         companyType: '2',
         createdAt: '2020-10-29 21:20:33',
-        updatedAt: '2020-10-29 21:20:33'
+        updatedAt: '2020-10-29 21:20:33',
       },
       {   
         id: 2,
         cnpj: '00000000000002',
         companyType: '1',
         createdAt: '2020-10-29 21:20:33',
-        updatedAt: '2020-10-29 21:20:33'
+        updatedAt: '2020-10-29 21:20:33',
       },
     ]);
   },
--- backend/src/database/seeders/20221228144634-users.js
+++ backend/src/database/seeders/20221228144634-users.js
@@ -5,14 +5,10 @@ module.exports = {
         id: 1,
         name: 'ALLAN SOUZA',
         email: 'allan@cashforce.com.br',
-        // phoneNumber: NULL,
-        // mobile: NULL,
-        // departament: NULL,
-        // verificationCode: '',
         emailChecked: 1,
         createdAt: '2020-10-01 21:31:37',
         updatedAt: '2020-10-01 22:41:23',
-        cashforceAdm: 1
+        cashforceAdm: 1,
       },
     ]);
   },
