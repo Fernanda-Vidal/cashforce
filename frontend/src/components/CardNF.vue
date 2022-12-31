30c08f096a482698d300ee9a2a0e5accddb3947c
Fernanda Vidal <fevidal.dev@gmail.com>
2022-12-31 18:52:32 -0300

    feat: cards NF e headerPage

---
+++ frontend/src/components/CardNF.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-card
+    class="mx-auto"
+    outlined
+    width=95%
+  >
+  <div class="card">
+
+  <span>{{info}}</span>
+  <span>{{info}}</span>
+  <span>{{info}}</span>
+  <span>{{info}}</span>
+  <span>{{info}}</span>
+  <span>{{info}}</span>
+  <button>Dados do cedente</button>
+  </div>
+  
+  </v-card>
+</template>
+
+<script>
+  export default {
+    props: {
+        info:  String,
+    }
+  }
+</script>
+
+<style scoped>
+    .card {
+        /* background-color: brown; */
+        display: flex;
+        justify-content:space-between;
+        margin-left: 55px;
+        margin-right: 25px;        
+    }
+
+    .mx-auto {
+        padding: 10px 15px 10px 15px;
+        margin-bottom: 15px;
+    }
+
+
+</style>
---
+++ frontend/src/views/NotasFiscais.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <HeaderNF />
+        <CardNF info="oi" />
+        <CardNF info="oi" />
+        <CardNF info="oi" />
+        <CardNF info="oi" />
+        <CardNF info="oi" />
+
+  </div>
+</template>
+
+<script>
+import HeaderNF from "../components/HeaderNF.vue";
+import CardNF from "../components/CardNF.vue";
+export default {
+  components: {
+    HeaderNF,
+    CardNF,
+  },
+};
+</script>
+
+<style scoped>
+</style>
